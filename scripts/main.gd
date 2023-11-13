ca6879411376b2a1a06450bd5e2aa081a9cec065
Jakub Kaczmarek <jakub.kaczmarek.888@gmail.com>
2023-11-13 18:19:22 +0100

    Add base project

---
+++ scripts/main.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+@onready var player_start_position: Marker2D = $PlayerStartPosition
+@onready var player: CharacterBody2D = $Player
+
+func _ready() -> void:
+	player.position = player_start_position.position
---
+++ scripts/player/player_controller.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+@export var friction: float = 7.0
+@export var acceleration: float = 10.0
+
+var direction: Vector2
+var screen_size: Vector2
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta: float) -> void:
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction != Vector2.ZERO:
+		velocity = lerp(velocity, direction.normalized() * speed, acceleration * delta)
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, friction * delta)
+
+	position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_slide()
