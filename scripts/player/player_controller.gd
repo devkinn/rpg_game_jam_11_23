e2a164b3f0c13c9390d14ee4df9b68c414088413
Matiaks <87781094+Matiaks@users.noreply.github.com>
2023-11-18 21:59:30 +0100

    Base player interaction (#6)
    * New assets imported
    
    * POOR shot modifiers service
    
    * intermittent fire fixed

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -4,7 +4,16 @@ extends CharacterBody2D
 @export var speed: float = 300.0
 @export var friction: float = 7.0
 @export var acceleration: float = 10.0
+@onready var single_fire = [$ProjectileStartPoint]
+@onready var double_fire = [$DoubleStartPoint1,$DoubleStartPoint2]
+@onready var triple_fire = [$DoubleStartPoint1,$ProjectileStartPoint,$DoubleStartPoint2]
+@onready var shooting_mode = [single_fire, double_fire, triple_fire]
+@onready var shoot_cooldown: Timer = $ShootCooldown
+@export var projectile_scene: PackedScene
+@export var projectile_speed = 500.0
 
+
+var mode = 0
 var direction: Vector2
 var screen_size: Vector2
 
@@ -20,6 +29,7 @@ var upgrade_options = []
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
@@ -32,7 +42,30 @@ func _physics_process(delta: float) -> void:
 
 	position = position.clamp(Vector2.ZERO, screen_size)
 	move_and_slide()
+	
+	if(Input.is_action_pressed("shoot") and shoot_cooldown.is_stopped()):
+		shoot()
+	
+func shoot():
+	for x in shooting_mode[mode]:
+		var projectile = projectile_scene.instantiate()
+		projectile.position = x.position
 		
+		var velosity = Vector2(0.0, -projectile_speed)
+		projectile.linear_velocity = velosity
+		add_child(projectile)
+	shoot_cooldown.start()	
+			
+func _unhandled_input(event):
+	if(event.is_action_pressed("upgrade")):
+		mode = (mode+1)%shooting_mode.size()
+	if(event.is_action_pressed("attack_speed_up")):
+		shoot_cooldown.wait_time /= 2
+	if(event.is_action_pressed("attack_speed_down")):
+		shoot_cooldown.wait_time *= 2
+	
+
+
 	
 func upgrade_player(upgrade) -> void:
 	upgrade_options.clear()
@@ -69,3 +102,4 @@ func level_up():
 	
 func _on_button_pressed() -> void:
 	progression_system.add_xp(randi_range(3, 5))
+
