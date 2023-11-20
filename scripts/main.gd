b519673ca24b35d2877797db33bb9c97d25b7519
0Artur1 <artur.strz@interia.pl>
2023-11-20 20:56:21 +0100

    hp visualisation

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,18 @@
 extends Node
 
+signal lost
+
 @onready var player: CharacterBody2D = $Player
 @onready var player_start_position: Marker2D = $PlayerStartPosition
 @onready var projectile: RigidBody2D = preload("res://scenes/projectile.tscn").instantiate()
 func _ready() -> void:
 	player.position = player_start_position.position
-
+	$HealthIcon/healthvalue.text = str($Player.Health)
+func _process(delta):
+	$HealthIcon/healthvalue.text = str($Player.Health)
 
 func game_over():
+	lost.emit()
 	player.Health = 3
 	player.shot_number = 1
 	player.upgrade_options.clear()
@@ -18,5 +23,6 @@ func game_over():
 	projectile.set_script(load("res://scripts/player/projectile.gd"))
 	player.get_node("Immunity").stop()
 	player.position = player_start_position.position
+	
 	
 
