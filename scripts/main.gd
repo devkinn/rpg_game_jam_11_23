a1b3e35974669013a644a445a980254ecd82899a
Matiaks <87781094+Matiaks@users.noreply.github.com>
2023-11-20 01:04:35 +0100

    health and game reset systems (#11)

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,21 @@ extends Node
 
 @onready var player: CharacterBody2D = $Player
 @onready var player_start_position: Marker2D = $PlayerStartPosition
-
+@onready var projectile: RigidBody2D = preload("res://scenes/projectile.tscn").instantiate()
 func _ready() -> void:
 	player.position = player_start_position.position
+
+
+func game_over():
+	player.Health = 3
+	player.shot_number = 1
+	player.upgrade_options.clear()
+	player.collected_upgrades.clear()
+	player.get_node("ProgressionSystem").current_level = 0
+	player.get_node("ProgressionSystem").current_xp = 0
+	player.get_node("ProgressionSystem").xp_needed_for_next_level = 10
+	projectile.set_script(load("res://scripts/player/projectile.gd"))
+	player.get_node("Immunity").stop()
+	player.position = player_start_position.position
+	
+
